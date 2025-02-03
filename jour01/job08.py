b51ba4cd851253ef9b0f2c27ff29cc9118e54dae
Thibault Caron <thibault.caron@laplateforme.io>
2025-02-03 14:23:27 +0100

    feat: jour01, job08

---
+++ jour01/job08.py
@@ -0,0 +1,32 @@
+import math
+
+class Cercle:
+    def __init__(self, rayon = 0.0):
+        self.rayon = rayon
+
+    def changer_rayon(self, nouveau_rayon):
+        self.rayon = nouveau_rayon
+        return self.rayon
+    
+    def afficher_infos(self):
+        print(f"Rayon : {self.rayon}")
+        print(f"Diamètre : {self.diametre()}")
+        print(f"Circonférence : {self.circonference()}")
+        print(f"Aire : {self.aire()}")
+
+    def circonference(self):
+        return 2 * math.pi * self.rayon
+
+    def aire(self):
+        return math.pi * (self.rayon ** 2)
+
+    def diametre(self):
+        return 2 * self.rayon
+
+
+if __name__ == "__main__":
+    cercle1 = Cercle(4)
+    cercle1.afficher_infos()
+
+    cercle2 = Cercle(7)
+    cercle2.afficher_infos()
