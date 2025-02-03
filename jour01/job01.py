558939b5c8f32bcdabca93ec240ac4df026528a5
Thibault Caron <thibault.caron@laplateforme.io>
2025-02-03 09:57:04 +0100

    Jour01, Job01

---
+++ jour01/job01.py
@@ -0,0 +1,8 @@
+class Operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+if __name__ == "__main__":
+    operation1 = Operation(0, 1)
+    print(operation1)
